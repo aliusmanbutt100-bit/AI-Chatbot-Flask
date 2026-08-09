f182d49fff715497014fa723fa6e12498595335f
aliusmanbutt100-bit <aliusmanbutt100@>
2026-08-08 23:03:25 -0700

    fix deployment

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ def chat():
 #this is for live deployment on railway
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
